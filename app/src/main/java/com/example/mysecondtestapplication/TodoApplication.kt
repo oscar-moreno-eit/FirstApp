fc24a7c71442a30cf0bb1b5261d51368f764e236
Oscar Gustavo Moreno Bautista <oscar.moreno@enhanceit.us>
2022-10-14 13:27:05 -0400

    Adding a new object application

---
+++ app/src/main/java/com/example/mysecondtestapplication/TodoApplication.kt
@@ -0,0 +1,14 @@
+package com.example.mysecondtestapplication
+
+import android.app.Application
+
+/**
+ * First object created in your object
+ * Use case:
+ * To initialize libraries (any labraries, Dagger, Logging libraries, analytics)
+ * Provide access to the application
+ */
+
+class TodoApplication: Application() {
+
+}
